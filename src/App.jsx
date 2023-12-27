b669f4bd5f1a8bf924b047071df5a8b2974253bf
EMRAN YONAS YIMER <emranyonas602@gmail.com>
2023-12-28 00:10:34 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+    <div className='container'>
+      <img src="public/image-qr-code.png" alt="" />
+      <h2 className="huge">Impove your front-end skills by building projects</h2>
+      <p className="small-text">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next leve
+      </p>
+    </div>
+    
+  )
+}
+
+export default App
